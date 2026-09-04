839cfd19d9f9f57cf4c0f9d478269e73f53c8e96
milosavvvv <fairhitsbusiness@gmail.com>
2026-09-04 20:29:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs.plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lolcoaching/', // Must match your GitHub repository name exactly
+  root: 'client',
+  base: '/lolcoaching/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
 })
